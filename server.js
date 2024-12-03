4ffd0b30e409f5d69bb17ece4e775a0c75b1834c
SHREK19701 <jardaolaholah@gmail.com>
2024-12-03 16:28:28 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,55 +1,15 @@
 // Nactení modulu
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Middleware
-app.use(bodyParser.json());
-app.use(cors());
-
-// Testovací databáze (pro jednoduchost)
-let users = [];
+// Nastavení statických souboru
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Hlavní stránka
 app.get('/', (req, res) => {
-    res.send('Vítejte na serveru seznamky "SZN"!');
-});
-
-// Registrace uživatele
-app.post('/register', (req, res) => {
-    const { username, email, password } = req.body;
-    if (!username || !email || !password) {
-        return res.status(400).json({ message: 'Vyplnte všechna pole!' });
-    }
-
-    const userExists = users.some(user => user.email === email);
-    if (userExists) {
-        return res.status(400).json({ message: 'E-mail je již registrován.' });
-    }
-
-    const newUser = { username, email, password, coins: 150 };
-    users.push(newUser);
-    res.status(201).json({ message: 'Registrace probehla úspešne!', user: newUser });
-});
-
-// Prihlášení uživatele
-app.post('/login', (req, res) => {
-    const { email, password } = req.body;
-    const user = users.find(user => user.email === email && user.password === password);
-
-    if (!user) {
-        return res.status(401).json({ message: 'Nesprávný e-mail nebo heslo.' });
-    }
-
-    res.json({ message: 'Prihlášení úspešné!', user });
-});
-
-// Zobrazení všech uživatelu (pro úcely testování)
-app.get('/users', (req, res) => {
-    res.json(users);
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Spuštení serveru
