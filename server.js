6f386344e34b37ed0b899c20cf3f5204acf9c879
SHREK19701 <jardaolaholah@gmail.com>
2024-12-03 16:35:06 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -2,14 +2,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000;
-
-// Nastavení statických souboru
-app.use(express.static(path.join(__dirname, 'public')));
+const PORT = 10000;
 
 // Hlavní stránka
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname,'index.html'));
 });
 
 // Spuštení serveru
